384008948e6e39d8eee7d8466f1318876064d298
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-04-04 22:37:57 +0500

    intial UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './componets/Navbar'
+import UserDetails from './componets/UserDetails'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <UserDetails/>
+    </>
+  )
+}
+
+export default App
