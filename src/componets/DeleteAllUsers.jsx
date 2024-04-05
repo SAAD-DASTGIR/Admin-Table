80f2a38f68f5fed598f51e6a81f26af1683c66a4
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-04-06 02:24:33 +0500

    Added DeleteAllUser

--- src/componets/DeleteAllUsers.jsx
+++ src/componets/DeleteAllUsers.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { DeleteUsers } from "../store/slices/userSlice";
 export const DeleteAllUser = () => {
-  return <div>DeleteAllUser</div>;
+  const dispatch=useDispatch()
+  const deleteall=()=>{
+    dispatch(DeleteUsers())
+    console.log("dastgir")
+  }
+  return( 
+  <button
+    onClick={deleteall}
+
+  >
+    DeleteAllUser</button>)
 };
