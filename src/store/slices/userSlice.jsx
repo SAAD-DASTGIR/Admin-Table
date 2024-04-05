244157c4ab6351d0545ace2f4145d0858f8f4901
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-04-06 02:23:52 +0500

    Modified UserSlice

--- src/store/slices/userSlice.jsx
+++ src/store/slices/userSlice.jsx
@@ -3,9 +3,18 @@ const userSlice = createSlice({
     name:'users',
     initialState:[],
     reducers:{
-        addUsers(state,action){},
-        removeUsers(state,actions){},
-        DeleteUsers(state,action){},
+        addUsers(state,action){
+            state.push(action.payload)
+            console.log(action.payload)
+        },
+        removeUsers(state,action){
+            state.splice(action.payload,1)
+        },
+        DeleteUsers(state,action){
+           return []
+        },
     }
 })
-export default userSlice.reducer
+export default userSlice.reducer // store
+// export const addUsers.action
+export const {addUsers,removeUsers,DeleteUsers} = userSlice.actions //body
