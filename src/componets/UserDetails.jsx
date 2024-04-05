c9214c65a56916091b458e2029a1d4d098939f05
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-04-06 02:26:09 +0500

    Refactored UserDetails

--- src/componets/UserDetails.jsx
+++ src/componets/UserDetails.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { DeleteAllUser } from './DeleteAllUsers';
 import styled from "styled-components";
+import { fakeUserData } from "../api";
+import {useDispatch} from 'react-redux'
+import { addUsers } from "../store/slices/userSlice"; 
+import DisplayUser from "./DisplayUsers"
+
 
 const UserDetails = () => {
+  const dispatch= useDispatch()
+  const addNewUser = (name ) => {
+    dispatch(addUsers(name))
+  }
   return (
     <Wrapper>
       <div className="content">
         <div className="admin-table">
           <div className="admin-subtitle">List of User Details</div>
-          <button className="btn add-btn">Add New Users</button>
+          <button 
+          onClick={() => addNewUser(fakeUserData())}
+          className="btn add-btn">Add New Users</button>
         </div>
+        < DisplayUser/>
         <ul>
           {/* <li>Hi</li>
           <li>Hii</li> */}
